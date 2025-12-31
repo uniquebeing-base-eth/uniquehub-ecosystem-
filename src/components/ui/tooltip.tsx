d62a14ed368a5bafc5361b59bfcb07629d60e6ab
Uniquebeing <beingunique049@gmail.com>
2025-12-31 13:17:29 +0100

    Add missing import for Tooltip components

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,10 +1,11 @@
 
 import * as React from "react";
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
-
 import { cn } from "@/lib/utils";
 
+
 const TooltipProvider = TooltipPrimitive.Provider;
+
 
 const Tooltip = TooltipPrimitive.Root;
 
