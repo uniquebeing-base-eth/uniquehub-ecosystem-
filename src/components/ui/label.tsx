c1ac569be620d55608e68ec615c8a98bb5c8cae5
Uniquebeing <beingunique049@gmail.com>
2025-12-31 13:29:24 +0100

    Update label.tsx

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,7 +1,9 @@
 
+
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
 import { cva, type VariantProps } from "class-variance-authority";
+
 
 import { cn } from "@/lib/utils";
 
