172632f2e0f82410101cca9c2695ab2f690f3474
Uniquebeing <beingunique049@gmail.com>
2025-12-31 13:10:12 +0100

    Memoize wallet client in useViemClients hook
    Add memoization for wallet client based on address

--- src/hooks/useViemClients.tsx
+++ src/hooks/useViemClients.tsx
@@ -2,10 +2,12 @@
 import { useMemo } from 'react';
 import { usePublicClient, useWalletClient } from 'wagmi';
 
+
 export const useViemClients = (address?: `0x${string}`) => {
   const publicClient = usePublicClient();
   const { data: wc } = useWalletClient();
 
+  
   const walletClient = useMemo(() => {
     if (!address || !wc) return null;
     return wc;
