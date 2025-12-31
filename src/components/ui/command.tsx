84fc2c6afc5bc4641c2f83af682c0a4683f6ed5d
Uniquebeing <beingunique049@gmail.com>
2025-12-31 13:25:19 +0100

    Add imports for Dialog and utility functions

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -4,8 +4,10 @@ import { type DialogProps } from "@radix-ui/react-dialog";
 import { Command as CommandPrimitive } from "cmdk";
 import { Search } from "lucide-react";
 
+
 import { cn } from "@/lib/utils";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
+
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
