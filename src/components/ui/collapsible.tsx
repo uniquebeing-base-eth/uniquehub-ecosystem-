89372e1a5663def1ec0aa16944b9491a5c919cc5
Uniquebeing <beingunique049@gmail.com>
2025-12-31 13:24:49 +0100

    Add CollapsibleTrigger to collapsible component

--- src/components/ui/collapsible.tsx
+++ src/components/ui/collapsible.tsx
@@ -1,7 +1,9 @@
 
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
 
+
 const Collapsible = CollapsiblePrimitive.Root;
+
 
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
 
