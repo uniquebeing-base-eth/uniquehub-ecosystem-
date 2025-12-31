daec0dc579325982e065754aa768ef0cf5445178
Uniquebeing <beingunique049@gmail.com>
2025-12-31 13:11:41 +0100

    Add missing imports in alert-dialog.tsx

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -2,8 +2,10 @@
 import * as React from "react";
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 
+
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
+
 
 const AlertDialog = AlertDialogPrimitive.Root;
 
