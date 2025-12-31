d53ac0098846efd2fe21c0d81ed3518094ee3e43
Uniquebeing <beingunique049@gmail.com>
2025-12-31 13:20:38 +0100

    Add forwardRef to Table component

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,7 +1,9 @@
 
 import * as React from "react";
 
+
 import { cn } from "@/lib/utils";
+
 
 const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
   ({ className, ...props }, ref) => (
