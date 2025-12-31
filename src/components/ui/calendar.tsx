06216e0d79e8c1a5089299413927c07dc1deb873
Uniquebeing <beingunique049@gmail.com>
2025-12-31 13:15:18 +0100

    Updated calendar tx

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,4 +1,3 @@
-
 import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
@@ -6,7 +5,9 @@ import { DayPicker } from "react-day-picker";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
+
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+
 
 function Calendar({ className, classNames, showOutsideDays = true, ...props }: CalendarProps) {
   return (
