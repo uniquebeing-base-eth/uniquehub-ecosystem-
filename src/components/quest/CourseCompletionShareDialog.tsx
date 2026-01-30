874515cc43d24442b7566cecf8660e1ba44d5757
Uniquebeing <beingunique049@gmail.com>
2026-01-30 19:03:37 +0100

    Update CourseCompletionShareDialog.tsx

--- src/components/quest/CourseCompletionShareDialog.tsx
+++ src/components/quest/CourseCompletionShareDialog.tsx
@@ -1,5 +1,6 @@
 
 
+
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Trophy, Share2, X } from "lucide-react";
