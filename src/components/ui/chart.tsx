0ed1d315cbe9c9489396666fe1f0120a30517a44
Uniquebeing <beingunique049@gmail.com>
2026-01-30 19:29:50 +0100

    Update chart.tsx

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -9,6 +9,7 @@ import { cn } from "@/lib/utils";
 // Format: { THEME_NAME: CSS_SELECTOR }
 const THEMES = { light: "", dark: ".dark" } as const;
 
+
 export type ChartConfig = {
   [k in string]: {
     label?: React.ReactNode;
