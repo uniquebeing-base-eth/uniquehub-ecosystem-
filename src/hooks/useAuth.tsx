fb04b76ecd51f5478833a6c0b044fe1e7dfab05c
Uniquebeing <beingunique049@gmail.com>
2026-01-30 20:13:06 +0100

    Update useAuth.tsx

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,4 +1,5 @@
 
+
 import React, { useState, useEffect, createContext, useContext, ReactNode } from 'react';
 import { User, Session } from '@supabase/supabase-js';
 import { supabase } from '@/integrations/supabase/client';
